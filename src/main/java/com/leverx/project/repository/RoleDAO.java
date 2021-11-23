bedd32b69a4ba8b2e3b0232858d2fe24ff544274
ValeriyLiashuk <valeriyliashuk@gmail.com>
2021-11-23 20:16:33 +0300

    create repository for Role.java

---
+++ src/main/java/com/leverx/project/repository/RoleDAO.java
@@ -0,0 +1,8 @@
+package com.leverx.project.repository;
+
+import com.leverx.project.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleDAO extends JpaRepository<Role, Integer> {
+
+}
