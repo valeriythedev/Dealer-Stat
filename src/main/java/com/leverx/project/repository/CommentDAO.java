b4f5c0457e8d177b19758cb612d2eefd175c15a5
ValeriyLiashuk <valeriyliashuk@gmail.com>
2021-11-29 20:34:40 +0300

    create functional for Comments

--- src/main/java/com/leverx/project/repository/CommentDAO.java
+++ src/main/java/com/leverx/project/repository/CommentDAO.java
@@ -2,8 +2,14 @@ package com.leverx.project.repository;
 
 import com.leverx.project.model.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface CommentDAO extends JpaRepository<Comment, Integer> {
+
+    @Query( value = "select c FROM Comment c JOIN c.users u WHERE u.id = :userId" )
+    List<Comment> findAllCommentsById(Integer userId);
 }
