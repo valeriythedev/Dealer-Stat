4c6efd9e05515859ed5a0d382dcc29863630b4e7
ValeriyLiashuk <valeriyliashuk@gmail.com>
2021-11-29 20:38:36 +0300

    cleanup code

--- src/main/java/com/leverx/project/repository/UserDAO.java
+++ src/main/java/com/leverx/project/repository/UserDAO.java
@@ -8,6 +8,4 @@ import org.springframework.stereotype.Repository;
 public interface UserDAO extends JpaRepository<User, Integer> {
 
     User findByEmail(String email);
-
-    User findByEmailAndPassword(String email, String password);
 }
