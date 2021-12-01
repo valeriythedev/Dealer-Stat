a119687398884c23a8ee66c65015af8c4dbad0b3
ValeriyLiashuk <valeriyliashuk@gmail.com>
2021-12-01 20:41:21 +0300

    cleanup code

--- src/main/java/com/leverx/project/security/jwt/JwtTokenProvider.java
+++ src/main/java/com/leverx/project/security/jwt/JwtTokenProvider.java
@@ -1,7 +1,11 @@
 package com.leverx.project.security.jwt;
 
 import com.leverx.project.model.Role;
-import io.jsonwebtoken.*;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
