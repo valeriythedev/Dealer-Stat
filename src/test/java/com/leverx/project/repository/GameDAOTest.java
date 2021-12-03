af5c8294792927704ebe97216520e27b513490c1
ValeriyLiashuk <valeriyliashuk@gmail.com>
2021-12-03 19:38:54 +0300

    rename directory from "service" to "repository"

--- src/test/java/com/leverx/project/repository/GameDAOTest.java
+++ src/test/java/com/leverx/project/repository/GameDAOTest.java
@@ -1,8 +1,7 @@
-package com.leverx.project.service;
+package com.leverx.project.repository;
 
 import com.leverx.project.application.Application;
 import com.leverx.project.model.Game;
-import com.leverx.project.repository.GameDAO;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
--- src/test/java/com/leverx/project/repository/GameObjectDAOTest.java
+++ src/test/java/com/leverx/project/repository/GameObjectDAOTest.java
@@ -1,8 +1,7 @@
-package com.leverx.project.service;
+package com.leverx.project.repository;
 
 import com.leverx.project.application.Application;
 import com.leverx.project.model.GameObject;
-import com.leverx.project.repository.GameObjectDAO;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
